71e52efdf341055388714ae34590fcd8b2191eb1
fsatsuki <98732283+fsatsuki@users.noreply.github.com>
2023-05-22 08:39:41 +0900

    Fix download_vc (#14)
    * cdkのバージョンアップに伴い、addDependsOnがaddDependencyに変更
    
    * api documentのハードコーディング部分の修正
    
    * VCのダウンロードリンクがハードコーディングされている問題を修正
    
    * apiドキュメントに記載が漏れていたパラメータを追記

--- verifierwebapp/src/components/DownloadVc.vue
+++ verifierwebapp/src/components/DownloadVc.vue
@@ -6,9 +6,9 @@
       </v-col>
       <v-col cols="6">
         <v-text-field
-          v-model="identifier"
-          label="Identifier that issues JWT"
-          placeholder="0x..."
+          v-model="did_url"
+          label="did_url"
+          placeholder="did:ethr:0x5:0xD..."
         >
         </v-text-field>
       </v-col>
@@ -16,7 +16,7 @@
         <v-text-field
           v-model="svcName"
           label="service name"
-          placeholder="0x..."
+          placeholder="..."
         >
         </v-text-field>
       </v-col>
@@ -39,51 +39,72 @@
 
 <script>
 import { defineComponent } from "vue";
-//import { API } from 'aws-amplify';
+import { API } from 'aws-amplify';
 
 export default defineComponent({
   name: "DownloadVc",
   props: [],
   data() {
     return {
-      svcName: "test2",
-      identifier:
-        "0x0333a5b1f233f5963b27f6d6d23e27831c4566dfeeba8ee76d34dbef787d4deabf",
-      jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE2NzM4NjA3OTAsImlzcyI6ImRpZDpldGhyOjB4NToweDAzMzNhNWIxZjIzM2Y1OTYzYjI3ZjZkNmQyM2UyNzgzMWM0NTY2ZGZlZWJhOGVlNzZkMzRkYmVmNzg3ZDRkZWFiZiJ9.61Ep5lzEM1lZYrs_rkg2YXeTIeAbTPoJDR-KmWyt5tPKjZ8lYxMS-w6t0FHH0lI257COHVuKzDeuhR6DL0cIUQA",
+      svcName: "example",
+      did_url:
+        "did:ethr:0x5:0x03ef6c8d5434daee10f209283b14d2a244837f2881ba467f73a3adcab58f29d7e3",
+      jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE2ODQ0NzgwMjYsImlzcyI6ImRpZDpldGhyOjB4NToweDAzZWY2YzhkNTQzNGRhZWUxMGYyMDkyODNiMTRkMmEyNDQ4MzdmMjg4MWJhNDY3ZjczYTNhZGNhYjU4ZjI5ZDdlMyJ9.ybRbG8Twzj50zqog3D5nRaNb6jCF1PKVquRnC3AXuRb-Eq55Wakz-hESezlfgkLVJsUcTJZMe33y4eVnxUx9gwE",
       vc_json: "",
       vc_contents: "",
     };
   },
   methods: {
     async GET_VC() {
-      // S3の署名付きURLを取得。jwtが正しければ取得可能
-      const query = new URLSearchParams({
-        identifier: this.identifier,
-        svcName: this.svcName,
-      });
-      const res = await fetch(
-        `https://yw627e1np3.execute-api.ap-northeast-1.amazonaws.com/prod//download?${query}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: this.jwt,
-          },
-        }
-      );
-      const response_json = await res.json();
-      console.log(response_json.downloadUrl);
+      const identifier = this.did_url.split(":").slice(-1)[0]
 
-      // 署名付きURLを使ってVerifiable Credentialのダウンロード
-      const res_vc = await fetch(response_json.downloadUrl, {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
+      // DIDドキュメントを取得
+      const didDocument = await API.get("api", `/document`, {
+        queryStringParameters: {
+          did_url: this.did_url,
         },
       });
 
-      this.vc_json = await res_vc.json();
-      this.vc_contents = JSON.stringify(this.vc_json, null, "  ");
+      // S3の署名付きURLを取得。jwtが正しければ取得可能
+      const query = new URLSearchParams({
+        identifier: identifier,
+        svcName: this.svcName,
+      });
+
+      for (let index = 0; index < didDocument.didDocument.service.length; index++) {
+        if (didDocument.didDocument.service[index].type == this.svcName){
+          console.log(didDocument.didDocument.service[index].serviceEndpoint)
+
+          const holderEndpoint = `https://${didDocument.didDocument.service[index].serviceEndpoint.split("/").slice(0)[2]}`
+          console.log("holderEndpoint: ", holderEndpoint)
+          const res = await fetch(
+            `${holderEndpoint}/prod//download?${query}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: this.jwt,
+              },
+            }
+          );
+          const response_json = await res.json();
+          console.log(response_json.downloadUrl);
+
+          // 署名付きURLを使ってVerifiable Credentialのダウンロード
+          const res_vc = await fetch(response_json.downloadUrl, {
+            method: "GET",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          this.vc_json = await res_vc.json();
+          this.vc_contents = JSON.stringify(this.vc_json, null, "  ");
+
+        }
+        
+      }
+      
     },
     // VCをダウンロードする
     async download_vc() {
